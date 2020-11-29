7f23af9327953441738c3e4e7fdafcc682ed2cca
unknown <abhijai086@gmail.com>
2020-11-29 09:13:12 +0530

    Adding UI Project

---
+++ COVID-HEALTH-REGISTER/src/app/_services/patient.service.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '@environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PatientService {
+
+    constructor(private http: HttpClient) { }
+
+    getPatientList(): Observable<any> {
+        return this.http.get(`${environment.backendUrl}/getAllPatients`);
+    }
+ addPatient(patient: any): Observable<any> {
+        return this.http.post<any>('http://localhost:9000/addPatient', patient)
+    }
+}
---
+++ COVID-HEALTH-REGISTER/src/app/home/home.component.ts
@@ -0,0 +1,51 @@
+﻿import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+import { Patient } from '@app/_models';
+import { PatientService, UserService } from '@app/_services';
+import { Observable } from "rxjs";
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+declare var $ : any;
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    patientsList: Observable<Patient[]>;
+
+    loading = false;
+    addForm;
+    openPopup() {
+        $("#myModal").modal("show");
+    }
+    users: User[];
+constructor(private userService: UserService, private patientService: PatientService) { }
+
+    ngOnInit() {
+        this.loading = true;
+        this.reloadData();
+        this.addForm = new FormGroup({
+            firstName : new FormControl("", [Validators.required]),
+            lastName : new FormControl("", [Validators.required]),
+            hospitalName : new FormControl("", [Validators.required]),
+            contact : new FormControl("", [Validators.required]),
+result : new FormControl("", [Validators.required])
+          });
+      
+        this.userService.getAll().pipe(first()).subscribe(users => {
+            this.loading = false;
+            this.users = users;
+        });
+    }
+reloadData() {
+        this.patientsList = this.patientService.getPatientList();
+      }
+
+    addPatient(addForm){
+        console.log(addForm)
+        this.patientService.addPatient(addForm).subscribe(
+          (res) => console.log(res),
+          (err) => console.log(err)
+        );
+        $('#myModal').modal('toggle');
+    }
+
+}
